fb6e7f30c818b13808639496b9d88754b7e5ac3a
sboese1 <samboese3@gmail.com>
2022-02-26 20:04:39 -0600

    Changed the structure of how nodes are added and modified

--- Main.py
+++ Main.py
@@ -7,18 +7,31 @@ class Node:
 class linked_list:
     def __init__(self):
         self.head = None
+        self.tail = None
 
 
-def add_to_end(node):
-    if ll.head.data != None:
-        ll.head.next = node
-    else:
-        ll.head = node
+    def add_to_end(self, node):
+        if linked.head is None:
+            linked.head = node
+            return
+
+        final = linked.head
+        while final.next is not None:
+            final = final.next
+
+        final.next = node
 
 
-ll = linked_list()
-ll.head = Node()
+    def print_list(self):
+        temp = linked.head
 
+        while temp is not None:
+            print(temp.data)
+            temp = temp.next
+
+
+
+linked = linked_list()
 
 title = input("What is the name of your shopping list? ")
 answer = 'Y'
@@ -26,10 +39,8 @@ answer = 'Y'
 while answer.upper() == 'Y':
     item = input("Enter the name of the item to put on the list: ")
     n = Node(item)
-    add_to_end(n)
+    linked.add_to_end(n)
     answer = input("Do you want to enter another item? ")
 
 
-while ll.head != None:
-    print(ll.head.data)
-    ll.head = ll.head.next
+linked.print_list()
