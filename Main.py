d8f4168fe47ce8c2566af4118646d7d107537038
sboese1 <samboese3@gmail.com>
2022-02-27 20:40:09 -0600

    Added some remove features to remove items in a list

--- Main.py
+++ Main.py
@@ -22,6 +22,36 @@ class linked_list:
         final.next = node
 
 
+    def remove_from_end(self):
+        if linked.head is None:
+            return
+
+        final = linked.head
+        while final.next.next is not None:
+            final = final.next
+
+        final.next = None
+
+
+    def remove_specific(self, item):
+        if linked.head is None:
+            return
+
+        if linked.head.next is None:
+            linked.head = None
+            return
+
+        current = linked.head.next
+        prev = linked.head
+        while current.next is not None:
+            if str(current.data) == item:
+                prev.next = current.next
+                return
+
+            current = current.next
+            prev = prev.next
+
+
     def print_list(self):
         temp = linked.head
 
@@ -41,6 +71,23 @@ while answer.upper() == 'Y':
     n = Node(item)
     linked.add_to_end(n)
     answer = input("Do you want to enter another item? ")
+    while answer.upper() != 'Y' and answer.upper() != 'N':
+        answer = input("Invalid input. Please input Y/N: ")
+
+
+remove = input("Do you want to remove an item? ")
+while remove.upper() == 'Y':
+    option = input("Do you want to remove the last item or a specific item? (L/S) ")
+    while option.upper() != 'L' and option.upper() != 'S':
+        option = input("Invalid input. Please input L/S: ")
+
+    if option.upper() == 'L':
+        linked.remove_from_end()
+    else:
+        spot = input("What item do you want to remove? ")
+        linked.remove_specific(spot)
+
+    remove = input("Do you want to remove an item? ")
 
 
 linked.print_list()
